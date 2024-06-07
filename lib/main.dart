73e182bd9eec6167f035b5711c63b7f0b35a1646
elenata <elena.a@iwmnetork.com>
2024-06-07 14:30:44 +0200

    changed theme color and font

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:expiremind/presentation/screens/login_screen.dart';
 import 'package:expiremind/presentation/screens/recipes_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'presentation/screens/prepare_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _selectedIndex = 0;
   final List<Widget> _screens = [
-    InventoryScreen(),
+    const InventoryScreen(),
     PrepareScreen(),
     RecipesScreen()
   ];
@@ -39,14 +40,45 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    MaterialColor customColor = const MaterialColor(0xFF2196F3, {
+      50: Color(0xFFE3F2FD),
+      100: Color(0xFFBBDEFB),
+      200: Color(0xFF90CAF9),
+      300: Color(0xFF64B5F6),
+      400: Color(0xFF42A5F5),
+      500: Color(0xFF2196F3),
+      600: Color(0xFF1E88E5),
+      700: Color(0xFF1976D2),
+      800: Color(0xFF1565C0),
+      900: Color(0xFF0D47A1),
+    });
+
     return MaterialApp(
       title: 'ExpiRemind',
-      theme: ThemeData(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: customColor,
+          brightness: Brightness.light,
+        ),
+
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.poppins(
+            fontSize: 30,
+          ),
+          bodyMedium: GoogleFonts.poppins(),
+          displaySmall: GoogleFonts.poppins(),
+        ),
+      ),
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           }
           if (snapshot.hasData) {
             return Scaffold(
